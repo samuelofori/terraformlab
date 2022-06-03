209f199e0c3af364378f06a851820491f8d65f56
Samuel Ofori <revsamuelofori@gmail.com>
2022-06-03 09:37:56 +0100

    Company Prefix Variables

--- var.tf
+++ var.tf
@@ -2,6 +2,13 @@
 variable "corp" {
 
     description = "naming convention for all resources"
-    default = "mcdonalds-incorporated"
+    default = "mcdonalds-incorporated" #specify the naming prefic for your environment - eg  Prod-rock
 
 }
+
+
+
+
+#terraform console
+#"${var.corp}-subnet-nw10-8er"
+#output from above command is  --> "mcdonalds-incorporated-subnet-nw10-8er"
